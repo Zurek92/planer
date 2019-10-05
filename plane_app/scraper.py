307739a991920088e01f22d2f4861ebf4e830cae
DawidBlonski <davidblonsky88@gmail.com>
2019-10-05 21:49:39 +0200

    added  selenium to docker and corrected scraper

--- plane_app/scraper.py
+++ plane_app/scraper.py
@@ -1,13 +1,19 @@
-from selenium import webdriver
 import datetime
-class Airport:
 
+from selenium import webdriver
+
+
+class Airport:
     def __init__(self):
-        options = webdriver.ChromeOptions()
-        options.add_argument('headless')
+        chrome_options = webdriver.ChromeOptions()
+        chrome_options.add_argument('--no-sandbox')
+        chrome_options.add_argument('--window-size=1420,1080')
+        chrome_options.add_argument('--headless')
+        chrome_options.add_argument('--disable-gpu')
 
         self.url = None
-        self.browser = webdriver.Chrome(executable_path='/home/dawid/selenium/chromedriver',options= options)
+        self.browser = webdriver.Chrome(options=chrome_options
+        )
         self.tags = ['tr', 'td']
         self.head = []
 
@@ -18,48 +24,43 @@ class Airport:
         for element in elements:
             element = element.find_elements_by_tag_name(self.tags[1])
             element = map(lambda x: x.text, element)
-            element = [x for x in element if x  ]
-            yield (dict(zip(self.head, element)))
-
+            element = [x for x in element if x]
+            yield dict(zip(self.head, element))
 
     @property
     def save_to_txt(self):
-        file = open(f'{self._class_name()}-{datetime.datetime.now().strftime("%d:%m:%Y %H:%M:%S")}.','w')
-        for data in self.get_data:
-            if data != {}:
-                file.write(f'{data}\n')
-        file.close()
+        with open(
+            f'{self._class_name()}-{datetime.datetime.now().strftime("%d:%m:%Y %H:%M:%S")}.', 'w'
+        ) as file:
+            for data in self.get_data:
+                if not data:
+                    file.write(f'{data}\n')
 
     def _class_name(self):
         return self.__class__.__name__
 
-class Chopina(Airport):
 
+class Chopina(Airport):
     def __init__(self):
         super().__init__()
         self.url = 'https://www.lotnisko-chopina.pl/pl/przyloty.html'
         self.head = ['flightboard', 'time', 'direction', 'flight', 'terminal', 'belt', 'status']
 
-class Mazury(Airport):
 
+class Mazury(Airport):
     def __init__(self):
         super().__init__()
         self.url = 'http://mazuryairport.pl/odloty_5.html'
         self.head = ['godz', 'data', 'rejs', 'kierunek', 'linia', 'status']
 
-class Krakow(Airport):
 
+class Krakow(Airport):
     def __init__(self):
         super().__init__()
-        self.url = 'http://www.krakowairport.pl/pl/pasazer,c70/informacje-o-lotach,c71/przyloty,a178.html'
+        self.url = (
+            'http://www.krakowairport.pl/pl/pasazer,c70/informacje-o-lotach,c71/przyloty,a178.html'
+        )
         self.head = ['godz', 'data', 'rejs', 'kierunek', 'linia']
 
-
-
-
-
-
-
-
-
-
+for i in Chopina().get_data:
+    print(i)
