c283083623c97aebf6f98632fa6be216cb97b0bc
bartoszkobylinski <bartosz.kobylinski@gmail.com>
2019-11-03 14:05:15 +0100

    fixing bug related ManyToMany Model

--- plane_app/models.py
+++ plane_app/models.py
@@ -4,9 +4,9 @@ from choices import choices
 
 # Create your models here.
 class AirCompany(models.Model):
-    air_company = models.CharField(max_length=50)
+    air_company_name = models.CharField(max_length=50)
     flight_number = models.CharField(max_length=10)
-    flight_status = models.CharField(choices=choices, default = ('on_time','on_time'))
+    flight_status = models.CharField( max_length = 50, choices=choices, default = ('on_time','on_time'))
     amount_of_delayed_flights_by_air_company = models.IntegerField()
     amount_of_cancelled_flights_by_air_company = models.IntegerField()
     amount_of_total_flights_by_air_company = models.IntegerField()
@@ -15,11 +15,9 @@ class Airport(models.Model):
     airport_name = models.CharField(max_length=50)
     # 3 letters code in IATA system ie. KRK, GDN
     airport_code = models.CharField(max_length=3)
-    air_company = models.ManyToManyField(AirCompany)
-    flight_number = models.ManyToManyField(AirCompany)
-    flight_status = models.ManyToManyField(AirCompany)
+    air_company_name = models.ManyToManyField(AirCompany)
     #arrival flights should have 1 as integer and departure flights should have 2
-    kind_of_flight = models.IntegerField(max_length=1)
+    kind_of_flight = models.IntegerField()
     amount_delayed_flights_on_airport = models.IntegerField()
     amount_cancelled_flights_on_airport = models.IntegerField()
     total_flights_on_airport = models.IntegerField()
